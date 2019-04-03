b4874df9d19f4710048eed71dde214d406490d62
Nathan Vack <njvack@tiamat.local>
2019-04-03 16:22:25 -0500

    Make a file lister demo

--- src/renderer/controllers/hello_controller.js
+++ src/renderer/controllers/hello_controller.js
@@ -1,12 +1,23 @@
 import { Controller } from "stimulus";
 
+const fs = require('fs');
+
 export default class extends Controller {
     static get targets() {
-        return ["name", "output"];
+        return ["path", "output"];
     }
 
-    greet() {
-        this.outputTarget.textContent =
-            `Hello, ${this.nameTarget.value}!`;
+    makelist() {
+        const dir = this.pathTarget.value;
+        const ul = this.outputTarget;
+        ul.innerHTML = '';
+        console.log(`listing ${dir}`);
+        const files = fs.readdir(dir, (err, files) => {
+            files.forEach((file, index) => {
+                let li = document.createElement('li');
+                li.textContent = file;
+                ul.appendChild(li);
+            });    
+        });
     }
 }
