c2d1bddd7f356bb14052cdcd6426f6c9905850ce
Nathan Vack <njvack@tiamat.local>
2019-04-03 11:38:05 -0500

    Initial commit

---
+++ src/renderer/controllers/hello_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+    static get targets() {
+        return ["name", "output"];
+    }
+
+    greet() {
+        this.outputTarget.textContent =
+            `Hello, ${this.nameTarget.value}!`;
+    }
+}
